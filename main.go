3557e209d916de5ec1c151c52d000f22d57d24f4
norbusonam <nsonam1822@gmail.com>
2023-11-25 11:34:39 -0500

    get and create simple string notes in memory

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "log"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
-	log.Println("Hello World")
+	var notes = []string{}
+
+	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			// convert notes to
+			notesJSON, err := json.Marshal(notes)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Write(notesJSON)
+		case "POST":
+			// check for empty note
+			var note = r.FormValue("note")
+			if note == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte("note is empty"))
+				return
+			}
+			notes = append(notes, note)
+			w.WriteHeader(http.StatusCreated)
+			w.Write([]byte(note))
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
+
 }
