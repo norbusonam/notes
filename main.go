17ee0ce4dbaaffca803e8132589f6810814b73ce
norbusonam <nsonam1822@gmail.com>
2023-11-25 11:48:52 -0500

    update to object from simple string

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
+type Note struct {
+	Note string `json:"note"`
+	Id   string `json:"id"`
+}
+
 func main() {
-	var notes = []string{}
+	var notes = []Note{}
 
 	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
-			// convert notes to
+			// convert notes to json list
 			notesJSON, err := json.Marshal(notes)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
@@ -20,16 +27,29 @@ func main() {
 			}
 			w.Write(notesJSON)
 		case "POST":
-			// check for empty note
-			var note = r.FormValue("note")
+			note := r.FormValue("note")
+			// check if note is empty
 			if note == "" {
 				w.WriteHeader(http.StatusBadRequest)
 				w.Write([]byte("note is empty"))
 				return
 			}
-			notes = append(notes, note)
+			// create new note
+			newNote := Note{
+				Note: note,
+				Id:   uuid.New().String(),
+			}
+			// convert new note to json
+			newNoteJSON, err := json.Marshal(newNote)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			// persist new note by appending to notes list
+			notes = append(notes, newNote)
 			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(note))
+			w.Write(newNoteJSON)
 		default:
 			w.WriteHeader(http.StatusMethodNotAllowed)
 		}
